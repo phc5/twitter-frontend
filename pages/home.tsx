ce6f0458fe731de7e04dea463ca58158d574c7bb
Paul <paulhyunchong@gmail.com>
2021-05-18 23:34:47 -0700

    Adds getProfile query

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import Layout from '../components/Layout';
-import HomeContent from '../components/home/HomeContent'
+import HomeContent from '../components/home/HomeContent';
 import RightNav from '../components/shared/Layout/RightNav';
 
 import { AuthContext } from '../context/AuthContext';
